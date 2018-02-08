bb75b0966ac5cb7dcca1d9b662af0ddd0d5f32a6
Stacie Taylor-Cima <stacietaylorcima@gmail.com>
2018-02-08 10:16:11 -0800

    write spec for GET about

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe WelcomeController, type: :controller do
       expect(response).to render_template("index")
     end
   end
+
+  describe "GET about" do
+    it "renders the about template" do
+      get :about
+      expect(response).to render_template("about")
+    end
+  end
 end
