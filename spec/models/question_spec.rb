797309004c87aed9eea38b6bc6dd1012c1eea372
Stacie Taylor-Cima <stacietaylorcima@gmail.com>
2018-02-13 21:01:24 -0800

    add resolve attribute to answer and question spec files

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body") }
+  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body", resolved: false) }
 
   describe "attributes" do
-    it "has title and body attributes" do
-      expect(question).to have_attributes(title: "New Question Title", body: "New Question Body")
+    it "should respond to title" do
+      expect(question).to respond_to(:title)
+    end
+
+    it "should respond to body" do
+      expect(question).to respond_to(:body)
+    end
+
+    it "should respond to resolved" do
+      expect(question).to respond_to(:resolved)
     end
   end
 end
