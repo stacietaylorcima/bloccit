9c48ff51a70c457d5e60aacdd0a06aa8a8ab14b8
Stacie Taylor-Cima <stacietaylorcima@gmail.com>
2018-02-14 13:03:49 -0800

    generate Posts controller, update routes, and update layout to reflect new paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get "welcome/index"
+  resources :posts
 
-  get "welcome/about"
+  get 'about' => 'welcome#about'
 
-  get "welcome/contact"
-
-  get "welcome/faq"
-
-  root "welcome#index"
-
+  root 'welcome#index'
 end
