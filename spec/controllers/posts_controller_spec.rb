a4914a6b83486d736b826b4a21c6aa7cca227800
Stacie Taylor-Cima <stacietaylorcima@gmail.com>
2018-02-17 13:54:46 -0800

    write tests to show posts

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -15,12 +15,23 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
-  # describe "GET #show" do
-  #   it "returns http success" do
-  #     get :show
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET show" do
+    it "returns http success" do
+      get :show, params: { id: my_post.id }
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #show view" do
+      get :show, params: { id: my_post.id }
+      expect(response).to render_template :show
+    end
+
+    it "assigns my_post to @post" do
+      get :show, params: { id: my_post.id }
+      expect(assigns(:post)).to eq(my_post)
+   end
+ end
+
   #
   # describe "GET #new" do
   #   it "returns http success" do
@@ -68,4 +79,4 @@ RSpec.describe PostsController, type: :controller do
       expect(response).to redirect_to Post.last
     end
   end
-end 
+end
