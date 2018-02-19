b3281c2f7f1407eb5e2be1b8228297164b718a33
Stacie Taylor-Cima <stacietaylorcima@gmail.com>
2018-02-18 22:00:59 -0800

    fix POST create tests to include params hash

--- spec/controllers/advertisements_controller_spec.rb
+++ spec/controllers/advertisements_controller_spec.rb
@@ -59,16 +59,16 @@ RSpec.describe AdvertisementsController, type: :controller do
 
   describe "POST create" do
     it "increases the number of advertisements by 1" do
-      expect{post :create, advertisement: {title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 99}}.to change(Advertisement,:count).by(1)
+      expect{ post :create, params: { advertisement: { title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 99 } } }.to change(Advertisement,:count).by(1)
     end
 
     it "assigns the new advertisement to @advertisement" do
-      post :create, advertisement: {title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 99}
+      post :create, params: { advertisement: { title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 99 } }
       expect(assigns(:advertisement)).to eq Advertisement.last
     end
 
     it "redirects to the new advertisement" do
-      post :create, advertisement: {title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 99}
+      post :create, params: { advertisement: { title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 99 } }
       expect(response).to redirect_to Advertisement.last
     end
   end
