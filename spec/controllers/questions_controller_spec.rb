d5cf2a350ed4b0e6e04e10d271fffc1ee9396971
Stacie Taylor-Cima <stacietaylorcima@gmail.com>
2018-02-21 12:57:28 -0800

    write red tests for question conrtoller GET index action and implement index action in question controller

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,29 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  let(:my_question) { Question.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, resolved: false) }
 
-  describe "GET #index" do
+  describe "GET index" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
-  end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+    it "assigns [my_question] to @questions" do
+      get :index
+      expect(assigns(:questions)).to eq([my_question])
     end
   end
 
-  describe "GET #new" do
+  describe "GET show" do
+    it "returns http success" do
+      get :show, params: { id: my_question.id }
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #show view" do
+      get :show, params: { id: my_question.id }
+      expect(response).to render_template :show
+    end
+
+    it "assigns my_question to @question" do
+      get :show, params: { id: my_question.id }
+      expect(assigns(:question)).to eq(my_question)
+    end
+  end
+
+  describe "GET new" do
     it "returns http success" do
       get :new
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #edit" do
+  describe "GET edit" do
     it "returns http success" do
       get :edit
       expect(response).to have_http_status(:success)
