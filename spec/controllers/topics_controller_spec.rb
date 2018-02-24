9eb8980e253d2d85d3807228fb42fe369b6e4ba2
Stacie Taylor-Cima <stacietaylorcima@gmail.com>
2018-02-24 12:34:59 -0800

    write tests for and implement topic's show view

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe TopicsController, type: :controller do
       name: RandomData.random_sentence,
       description: RandomData.random_paragraph
     )
-  end 
+  end
 
   describe "GET index" do
     it "returns http success" do
@@ -17,6 +17,23 @@ RSpec.describe TopicsController, type: :controller do
     it "assigns my_topic to @topics" do
       get :index
       expect(assigns(:topics)).to eq([my_topic])
+    end
+  end
+
+  describe "GET show" do
+    it "returns http success" do
+      get :show, params: { id: my_topic.id }
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #show view" do
+      get :show, params: { id: my_topic.id }
+      expect(response).to render_template :show
+    end
+
+    it "assigns my_topic to @topic" do
+      get :show, params: { id: my_topic.id }
+      expect(assigns(:topic)).to eq(my_topic)
     end
   end
 end
