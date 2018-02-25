7de47fd9158e09d582b6b48af6b6fc20c67771ef
Stacie Taylor-Cima <stacietaylorcima@gmail.com>
2018-02-24 19:20:11 -0800

    edit posts controller and specs to accommodate for post/topic association

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :topics
   resources :questions
   resources :advertisements
-  resources :posts
+  resources :topics do
+    resources :posts, except: [:index]
+  end
 
   get 'about' => 'welcome#about'
 
