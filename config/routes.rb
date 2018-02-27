6486c8e1dfb7b23020253ae7cec955a903c0b656
Stacie Taylor-Cima <stacietaylorcima@gmail.com>
2018-02-27 10:49:45 -0800

    Create and migrate user model, write specs, implement user model functionality, define user routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
     resources :sponsored_posts, except: [:index]
   end
+  resources :users, only: [:new, :create]
 
   get 'about' => 'welcome#about'
 
