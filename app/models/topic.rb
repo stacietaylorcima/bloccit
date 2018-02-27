9a684561048a1222c75d1c863cbe6c252bb66a8e
Stacie Taylor-Cima <stacietaylorcima@gmail.com>
2018-02-26 20:03:47 -0800

    write topic validation and tests, create a new topic form partial

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,7 @@
 class Topic < ApplicationRecord
   has_many :posts
-  has_many :sponsored_posts
+  has_many :sponsored_post
+
+  validates :name, length: { minimum: 5 }, presence: true
+  validates :description, length: { minimum: 15 }, presence: true
 end
