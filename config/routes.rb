4ea9dab599f4bb1430a607f396336ebb6e890f75
Stacie Taylor-Cima <stacietaylorcima@gmail.com>
2018-02-27 11:10:27 -0800

    add callback to user model that formats the user's name

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
     resources :sponsored_posts, except: [:index]
   end
-
+  resources :users, only: [:new, :create]
+  
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
