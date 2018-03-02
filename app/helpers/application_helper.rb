0214df8e82607c20558adcb23fc2e983175c4c4a
Stacie Taylor-Cima <stacietaylorcima@gmail.com>
2018-03-01 20:39:21 -0800

    add gravatar to application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,20 @@ module ApplicationHelper
     css_class << ' has-error' if errors.any?
     content_tag :div, capture(&block), class: css_class
   end
+
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+  end
+
+  # def avatar_url(user)
+  #   default_url = “app/assets/images/profile_default.png”
+  #
+  #   if(user.present?)
+  #     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+  #     "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+  #   else
+  #     return default_url
+  #   end
+  # end
 end
