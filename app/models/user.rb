92fe1616036276702138ac50997c1ea8b48fd5b1
Stacie Taylor-Cima <stacietaylorcima@Stacies-MacBook-Air.local>
2018-03-06 12:05:52 -0800

    add roles to users and allow/restrict access based on authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,16 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   before_save { self.email = email.downcase if email.present? }
-
+  before_save { self.role ||= :member }
   before_save :format_name
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
-
   validates :email,
     presence: true,
     uniqueness: { case_sensitive: false },
     length: { minimum: 3, maximum: 254 }
-
   has_secure_password
 
   def format_name
@@ -25,6 +22,6 @@ class User < ApplicationRecord
 
       self.name = name_array.join(" ")
     end
-
   end
+  enum role: [:member, :admin]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,18 @@ require 'random_data'
 end
 users = User.all
 
-user = User.first
-user.update_attributes!(
-  email: 'stacietaylorcima@gmail.com',
+# Create an admin user
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+# Create a member
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
   password: 'helloworld'
 )
 
