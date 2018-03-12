769ff6588740fc8f139e630f3e8d79aa56e22d71
Stacie Taylor-Cima <stacietaylorcima@Stacies-MacBook-Air.local>
2018-03-11 20:14:31 -0700

    create the first vote when a post is created

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,9 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
 
+  after_create :create_vote
+
   default_scope { order('rank DESC') }
-  scope :ordered_by_title, -> { reorder(title: ASC) }
-  scope :ordered_by_reverse_created_at, -> { reorder('created_at ASC') }
 
 
   validates :title, length: { minimum: 5 }, presence: true
@@ -31,4 +31,9 @@ class Post < ApplicationRecord
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
   end
+
+  private
+  def create_vote
+    user.votes.create(value: 1, post: self)
+  end 
 end
