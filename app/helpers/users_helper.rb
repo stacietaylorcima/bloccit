0b800d77b9dc17a88b28dd334adff7daa699d107
Stacie Taylor-Cima <stacietaylorcima@Stacies-MacBook-Air.local>
2018-03-15 10:04:45 -0700

    move post, comment, and favorite display code from user #show to user helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,25 @@
 module UsersHelper
+  def user_has_posts(user)
+    if @user.posts.any?
+      render @user.posts
+    else
+      "#{@user.name} has not submitted any posts yet."
+    end
+  end
+
+  def user_has_comments(user)
+    if @user.comments.any?
+      render @user.comments
+    else
+      "#{@user.name} has not submitted any comments yet."
+    end
+  end
+
+  def user_has_favorites(user)
+    if @user.favorites.any?
+      render partial: 'users/favorites'
+    else
+      "#{@user.name} has not favorited any posts yet."
+    end
+  end
 end
