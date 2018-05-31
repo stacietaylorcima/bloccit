7f5afd2e38d9fc933d7e024220202a8fd2b0d750
Stacie Taylor-Cima <stacietaylorcima@Stacies-MacBook-Air.local>
2018-05-31 10:25:58 -0700

    add trackstar tracking

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,16 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require_tree .
+var trackstar = {};
+trackstar.report = function(eventName){
+  // create an event JSON object to send to the TrackStar analytics service
+  var event = {event: { name: eventName }};
+  // initialize a new XMLHttpRequest() (AJAX request)
+  var request = new XMLHttpRequest();
+  // set the HTTP method to POST, the url of our events endpoint, and allow the request to run asynchronously
+  request.open("POST", "http://localhost:3000/api/events", true);
+  // set the request Content-Type header to 'application/json'
+  request.setRequestHeader('Content-Type', 'application/json');
+  // send the request
+  request.send(JSON.stringify(event));
+};
