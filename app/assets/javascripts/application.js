3606c668ed7f1e67195f4e866fcf3f4a08dd994d
Stacie Taylor-Cima <stacietaylorcima@Stacies-MacBook-Air.local>
2018-06-06 20:17:07 -0700

    add more tracksmart

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@ trackstar.report = function(eventName){
   var request = new XMLHttpRequest();
   // set the HTTP method to POST, the url of our events endpoint, and allow the request to run asynchronously
   request.open("POST", "https://shielded-garden-20003.herokuapp.com/api/events", true);
+  // request.open("POST", "http://localhost:3000/api/events", true);
   // set the request Content-Type header to 'application/json'
   request.setRequestHeader('Content-Type', 'application/json');
   // send the request
